65913d0da87e10b7ad7623b8cac89dff8829c6bf
Luuisaker <luisjqayala98@gmail.com>
2024-06-22 15:14:41 -0400

    primer commit

---
+++ Controllers/Nueva carpeta/ContactosControllers.js
@@ -0,0 +1,148 @@
+﻿require('dotenv').config();
+const express = require('express');
+let mysql = require("mysql");
+let { datos } = require('pg');
+const axios = require('axios');
+const nodemailer = require("nodemailer");
+const router = express.Router();
+router.use(express.urlencoded({ extended: true }));
+
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.hostinger.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: "test009@arodu.dev",
+        pass: "eMail.test009",
+    }
+});
+
+let conection = mysql.createConnection({
+    host: "dpg-cpqfpjo8fa8c739epsng-a",
+    user: "root",
+    password: "dGgfscPeVeaJpXMePlrwWgTqJNAuElNI",
+    database: "datos_gm0w"
+});
+
+router.post('/register', (req, res) => {
+    let datos = req.body;
+    let name = datos.name;
+    let email = datos.email;
+    let coment = datos.coment;
+    const currentDate = new Date();
+    const date = currentDate.toLocaleString();
+    const recaptchaResponse = req.body['g-recaptcha-response'];
+    const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS datos(
+        ID INT(100) PRIMARY KEY AUTO_INCREMENT,
+        Nombre VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+        Correo VARCHAR(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+        Comentario VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+        IP VARCHAR(24) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+        Fecha VARCHAR(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+        Locacion VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
+    );
+`;
+
+    function verifyRecaptcha(recaptchaResponse) {
+        const secretKey = process.env.recaptchaSecretKey;
+        axios.post('https://www.google.com/recaptcha/api/siteverify', {
+            secret: secretKey,
+            response: recaptchaResponse
+        }).then((response) => {
+            if (response.data.success) {
+                
+
+                res.send('Registration successful');
+            } else {
+                res.status(400).send('reCAPTCHA verification failed');
+            }
+        }).catch((error) => {
+            console.error('Error verifying reCAPTCHA:', error);
+            res.status(500).send('Internal server error');
+        });
+    }
+
+    if (!name || name.length <= 0) {
+        res.send(`
+    <script>
+      alert("Debe colocar un nombre!");
+      window.location.href = "/Formulario";
+    </script>
+  `);
+    }
+
+    if (!email || email.length <= 0) {
+        res.send(`
+    <script>
+      alert("Debe colocar un correo electrónico!");
+      window.location.href = "/Formulario";
+    </script>
+
+  `);
+
+    }
+    if (!coment || coment.length <= 0) {
+        res.send(` 
+    <script>
+      alert("Debe colocar un Comentario!");
+      window.location.href = "/Formulario";
+    </script>
+  `);
+    }
+
+    else {
+        axios.get(process.env.apiUrl)
+            .then(response => {
+                const { country_name, ip } = response.data;
+                const locacion = country_name;
+                const ip_adress = ip;
+                const nuevoregistro = "INSERT INTO datos(`Nombre`, `Correo`, `Comentario`, `IP`, `Fecha`, `Locacion`) VALUES('" + name + "', '" + email + "', '" + coment + "','" + ip_adress + "','" + date + "','" + locacion + "')";
+                conection.query(nuevoregistro, function (error, rows) {
+                    if (error) {
+                        throw error;
+                    } else {
+                        res.send(`
+    <script>
+      alert("Registro Completado!");
+      window.location.href = "/";
+    </script>
+  `);
+                        let mailOptions = {
+                            from: 'test009@arodu.dev',
+                            to: `luisjqayala98@gmail.com`,
+                            subject: 'Nuevo registro',
+                            text: `Nuevo registro:\nNombre: ${name}\nCorreo: ${email}\nComentario: ${coment}\nIP: ${ip_adress}\nFecha: ${date}\nLocación: ${locacion}`
+                        };
+                        transporter.sendMail(mailOptions, function (error, info) {
+                            if (error) {
+                                return console.log(error);
+                            }
+                            console.log('Email sent: ' + info.response);
+                        });
+                    }
+                });
+            })
+            .catch((error) => {
+                console.error('Error fetching location:', error);
+                res.status(500).send('Internal server error');
+            });
+    }
+});
+async function verifyRecaptcha(recaptchaResponse) {
+    const secretKey = recaptchaSecretKey;
+    try {
+        const response = await axios.post('https://www.google.com/recaptcha/api/siteverify', {
+            secret: secretKey,
+            response: recaptchaResponse
+        });
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,172 @@
+require('dotenv').config();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var ContactosController = require('./Controllers/ContactosControllers');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const passport = require('passport');
+const session = require('express-session');
+const mongoose = require("mongoose");
+let mysql = require("mysql");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require('mongoose-findorcreate');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(ContactosController);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+const uri = process.env.DB_URL;
+
+const client = new mongoose.Mongoose();
+
+async function run() {
+    try {
+        await mongoose.connect(uri, {
+            serverApi: "1"
+        });
+        await mongoose.connection.db.admin().command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+run().catch(console.error);
+const Schema = mongoose.Schema;
+const usuarioSechema = new Schema({
+    username: String,
+    googleId: String,
+});
+
+usuarioSechema.plugin(passportLocalMongoose);
+usuarioSechema.plugin(findOrCreate);
+const Usuario = mongoose.model("Usuario", usuarioSechema);
+passport.use(Usuario.createStrategy());
+
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, { id: user.id });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.Client_ID,
+    clientSecret: process.env.Client_Secret,
+    callbackURL: process.env.Client_URL
+},
+    async function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        try {
+            const user = await Usuario.findOne({ googleId: profile.id });
+            return cb(null, user);
+        } catch (err) {
+            return cb(err, null);
+        }
+    }));
+app.route("/Login")
+    .get(function (req, res) {
+        res.render("Login");
+    });
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+app.route("/auth/google/contactos")
+    .get(passport.authenticate('google', { failureRedirect: "/Login" }),
+        function (req, res) {
+            res.redirect("/contactos");
+        });
+
+
+
+app.route("/Formulario")
+    .get(function (req, res) {
+        res.render("Formulario");
+    });
+app.route("/cv")
+    .get(function (req, res) {
+        const data = {
+            title: 'Curriculum',
+            Name: 'Luis',
+            Lastname: 'Ayala',
+            Mail: 'Luisjqayala98@gmail.com',
+            School: 'U.E.C.A "Angel De La Guarda" ',
+            Phone: '04145861216',
+        };
+        res.render('cv', data);
+    });
+
+app.route("/contactos")
+    .get(function (req, res) {
+        if (req.isAuthenticated()) {
+            const connection = mysql.createConnection({
+                host: process.env.SQLHOST,
+                user: process.env.SQLUSER,
+                password: process.env.SQLPASSWORD,
+                database: process.env.SQLDatabase
+            });
+
+            connection.connect((err) => {
+                if (err) {
+                    console.error("Error connecting to database:", err);
+                    res.status(500).send("Error connecting to database");
+                    return;
+                }
+                connection.query("SELECT * FROM datos", (err, rows, fields) => {
+                    if (err) {
+                        console.error("Error running query:", err);
+                        res.status(500).send("Error running query");
+                        return;
+                    }
+
+                    const data = {
+                        title: 'contactos',
+                        records: rows,
+                    };
+
+                    res.render("contactos", data);
+                    connection.end();
+                });
+            });
+        } else {
+            res.render("login");
+        }
+    });
+
+
+
+
+
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
